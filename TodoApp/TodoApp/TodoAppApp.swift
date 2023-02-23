27b9301765661ec3103af529524aa0bfc5660208
flashseok <flashseok@gmail.com>
2023-02-24 00:57:35 +0900

    Todo App First Commit

---
+++ TodoApp/TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by 김현석 on 2023/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
